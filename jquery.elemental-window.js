96130dd0ba294a3341077146a8d55a1006254ffa
Sean Hill <sean.daryl.hill@gmail.com>
2013-09-03 11:55:51 -0600

    Added initial jquery plugin for elemental-window

---
+++ jquery.elemental-window.js
@@ -0,0 +1,24 @@
+(function ( $, w ) {
+
+	$.fn.toWindowEdges = function() {
+
+		var elem = $(this);
+		var elemHeight = elem.outerHeight();
+		var elemWidth = elem.outerWidth();
+		var edge = edge || "top";
+
+		var windowHeight = $(w).height();
+		var windowWidth = $(w).width();
+
+		var edges = {};
+
+       	edges.top = elem.offset().top;
+    	edges.right =  windowWidth - elemWidth - elem.offset().left;
+    	edges.bottom =  windowHeight - elemHeight - elem.offset().top;
+        edges.left = elem.offset().left;
+
+        return edges;
+ 
+    };
+
+})( jQuery, window );
